c0d41bde631d2688c6b601e024c0b36207031879
YZBruh <yagizzengin73@gmail.com>
2023-12-28 17:21:59 +0300

    gsı: adapte awakenos

--- awaken.mk
+++ awaken.mk
@@ -1,7 +1,7 @@
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-$(call inherit-product, vendor/evolution/config/BoardConfigSoong.mk)
-$(call inherit-product, device/evolution/sepolicy/common/sepolicy.mk)
--include vendor/evolution/build/core/config.mk
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+$(call inherit-product, vendor/awaken/config/BoardConfigSoong.mk)
+$(call inherit-product, device/awaken/sepolicy/common/sepolicy.mk)
+-include vendor/awaken/build/core/config.mk
 
 BOARD_EXT4_SHARE_DUP_BLOCKS := true
 
@@ -10,4 +10,4 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/ponces/treble_build_awaken/udc/ota.json
+    ro.system.ota.json_url=https://raw.githubusercontent.com/YZBruh/treble_build_awaken/udc/ota.json
